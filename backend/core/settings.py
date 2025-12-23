bac3fb007d16c5f59d6a2772c11a8f8240e92ceb
vipinpandey1804 <vipinpandey1804@gmail.com>
2025-12-23 17:36:30 +0530

    add db

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -144,14 +144,23 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'hAGKXnGHFDnGcyxtLmUvvZmhTYbLLwNx',
+        'HOST': 'yamanote.proxy.rlwy.net',
+        'PORT': '10485',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
